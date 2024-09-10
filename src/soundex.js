5cf94265cd5b9607771348cbc7fcd3d5e21a860b
arpithaP <163737012+arpithaBosch@users.noreply.github.com>
2024-09-10 14:07:00 +0530

    Update soundex.js

--- src/soundex.js
+++ src/soundex.js
@@ -1,39 +1,57 @@
 function getSoundexCode(char) {
-    char = char.toUpperCase();
-    const soundexDict = {
-        'B': '1', 'F': '1', 'P': '1', 'V': '1',
-        'C': '2', 'G': '2', 'J': '2', 'K': '2', 'Q': '2', 'S': '2', 'X': '2', 'Z': '2',
-        'D': '3', 'T': '3',
-        'L': '4',
-        'M': '5', 'N': '5',
-        'R': '6'
-    };
-    return soundexDict[char] || '0';
+  const soundexDict = {
+    B: '1', F: '1', P: '1', V: '1',
+    C: '2', G: '2', J: '2', K: '2', Q: '2', S: '2', X: '2', Z: '2',
+    D: '3', T: '3',
+    L: '4',
+    M: '5', N: '5',
+    R: '6',
+  };
+  return soundexDict[char.toUpperCase()] || '0';
+}
+
+function getFirstLetter(name) {
+  return name[0].toUpperCase();
+}
+
+function addCodeIfValid(code, prevCode) {
+  return code !== '0' && code !== prevCode ? code : null;
+}
+
+function padSoundex(soundex) {
+  while (soundex.length < 4) {
+    soundex.push('0');
+  }
+  return soundex.join('');
+}
+
+function cleanName(name) {
+  return name.trim().replace(/[^A-Za-z]/g, '');
+}
+
+function processPart(part) {
+  const cleanedPart = cleanName(part);
+  if (!cleanedPart) return '0000';
+
+  const soundex = [getFirstLetter(cleanedPart)];
+  let prevCode = getSoundexCode(soundex[0]);
+
+  for (let i = 1; i < cleanedPart.length && soundex.length < 4; i++) {
+    const code = getSoundexCode(cleanedPart[i]);
+    const validCode = addCodeIfValid(code, prevCode);
+    if (validCode) soundex.push(validCode);
+    prevCode = code;
+  }
+
+  return padSoundex(soundex);
 }
 
 function generateSoundex(name) {
-    if (!name) return '';
-
-    let soundex = [name[0].toUpperCase()];
-    let prevCode = getSoundexCode(name[0]);
-
-    for (let i = 1; i < name.length && soundex.length < 4; i++) {
-        let code = getSoundexCode(name[i]);
-        if (code !== '0' && code !== prevCode) {
-            soundex.push(code);
-        }
-        prevCode = code;
-    }
-
-    while (soundex.length < 4) {
-        soundex.push('0');
-    }
-
-    return soundex.join('');
+  if (!name) return '0000';
+  return processPart(name);
 }
 
 module.exports = {
-    getSoundexCode,
-    generateSoundex
+  getSoundexCode,
+  generateSoundex,
 };
-
